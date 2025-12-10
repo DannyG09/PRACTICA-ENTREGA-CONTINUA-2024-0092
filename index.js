0f4087d1e877f05e158767f3c4bb33b0d6654578
DannyG09 <dannyguerrero.m53@gmail.com>
2025-12-10 12:11:28 -0400

    prueba deploy automático

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hola Mundo desde DevOps! Danny Enmanuel Guerrero Martinez 2024-0092");
+    res.send("Hola Mundo desde DevOps! Danny Enmanuel Guerrero Martinez (2024-0092)");
 });
 
 const PORT = 3000;
