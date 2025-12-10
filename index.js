3de3bac8cc3556112aed1c3255f5946c2169ec94
DannyG09 <dannyguerrero.m53@gmail.com>
2025-12-10 11:42:26 -0400

    Proyecto practica cont

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hola Mundo desde DevOps! Danny Enmanuel Guerrero Martinez 2024-0092");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
